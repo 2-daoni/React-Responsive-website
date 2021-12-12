bd1848122238a43d8da7332eb1eb6dad2974a5ec
dawon <lyumilove9@gmail.com>
2021-12-12 16:52:36 +0900

    favicon 수정, class명 수정, profile-second 제작시작

--- src/Container/Home/Profile.js
+++ src/Container/Home/Profile.js
@@ -5,10 +5,10 @@ import './Profile.css';
 export default function Profile() {
   return (
     <div className="profile-container">
-      <div className="profile-parent">
+      <div className="profile-first">
         <div className="profile-details">
-          <div className="colz">
-            <div className="colz-icon">
+          <div className="social">
+            <div className="social-icon">
               <a href="https://daoni.notion.site/4208a9762f3a4a559c7eb67c79bc72fe">
                 <i className="fas fa-book"></i>
               </a>
@@ -22,24 +22,23 @@ export default function Profile() {
           </div>
           <div className="profile-text">
             <div className="profile-details-name">
-              <span className="primary-text ">
+              <h3 className="name">
                 안녕하세요, 저는 <span className="highlighted-text ">이다원</span> 입니다.
-              </span>
+              </h3>
             </div>
-            <div className="profile-details-role">
-              <span className="primary-text">
+            <div className="profile-details-dev">
+              <span className="dev">
                 {' '}
                 <h1>
                   저는&nbsp;
                   <Typical loop={Infinity} wrapper="b" steps={['프론트엔드👩', 2000, '노력하는🔥', 2000, '성장하는💦', 2000]} />
                   &nbsp;개발자 입니다.
                 </h1>
-                <span className="profile-role-tagline description">프론트엔드 개발자를 꿈꾸는 오니입니다</span>
+                <span className="description">프론트엔드 개발자를 꿈꾸는 오니입니다</span>
               </span>
             </div>
           </div>
-
-          <div className="profile-options">
+          <div className="profile-btn">
             <button className="btn primary-btn">
               {''}
               함께 일하고싶어요
@@ -49,8 +48,17 @@ export default function Profile() {
             </a>
           </div>
         </div>
+
         <div className="profile-pic">
           <div className="profile-pic-background"></div>
+        </div>
+      </div>
+      <div className="profile-second">
+        <div className="skills">
+          <h3>Html</h3>
+          <h3>CSS</h3>
+          <h3>JavaScript</h3>
+          <h3>React</h3>
         </div>
       </div>
     </div>
